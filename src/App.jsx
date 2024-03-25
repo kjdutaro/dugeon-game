5ba459c71895266f5d40be1538a82b8166e486aa
kjdutaro <kjdutaro@gmail.com>
2024-03-25 09:30:29 +0800

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h1>Odin's Dungeon</h1>
+    </div>
+    <form action="">
+      <input type="text" placeholder="Enter name:" />
+      <button>Play Game</button>
+    </form>
+    </>
+  )
+}
+
+export default App
